1dcef97bddceab61afb3d674f3e0c4716fe9449f
Sachin Srivastava <sachinnephrology@gmail.com>
2026-05-26 23:42:48 +0530

    fix: linearize alembic migrations branch heads

--- alembic/versions/0015_idh_prediction_fields.py
+++ alembic/versions/0015_idh_prediction_fields.py
@@ -16,7 +16,7 @@ from alembic import op
 from sqlalchemy.engine.reflection import Inspector
 
 revision: str = "0015"
-down_revision: Union[str, None] = "28e0c191bf7a"
+down_revision: Union[str, None] = "0014"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
